92f92c8e1bfe963478e0812911775225e1c4391c
justdave001 <81288192+justdave001@users.noreply.github.com>
2021-05-24 14:30:14 -0500

    added static root

--- Datsa/settings.py
+++ Datsa/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = BASE_DIR / 'static',
+STATICFILES_DIRS = BASE_DIR / 'staticfiles',
 
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
