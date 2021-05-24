c41bf23afb92549446a6838158abcab107bcbb9e
justdave001 <81288192+justdave001@users.noreply.github.com>
2021-05-24 13:38:47 -0500

    Going online with heroku

--- Datsa/settings.py
+++ Datsa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
@@ -122,7 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = BASE_DIR / 'static',
+
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+herok
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn Datsa.wsgi
