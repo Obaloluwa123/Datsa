5615eb4ac4907d83eb066b022a04452b8db10568
justdave001 <81288192+justdave001@users.noreply.github.com>
2021-05-24 14:46:05 -0500

    Initial

--- Datsa/settings.py
+++ Datsa/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = BASE_DIR / 'staticfiles',
+STATICFILES_DIRS = BASE_DIR / 'static',
 
 django_heroku.settings(locals())
 # Default primary key field type
